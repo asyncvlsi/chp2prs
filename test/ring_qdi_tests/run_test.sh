db32690f6cf7c78bbab3e7ac7eb6b511606b3526
Karthi Srinivasan <87223671+karthisrinivasan@users.noreply.github.com>
2024-12-12 21:54:49 -0500

    qdi datapath and tests

---
+++ test/ring_qdi_tests/run_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+REF="-ref=1"
+#REF=
+
+file=$1
+
+fail=0
+
+export ACT_PATH=runs
+
+actsim $REF ${file}tst.act test <<EOF > runs/${file}_sim.out 2>&1
+mode reset
+set Reset 1
+set GND 0
+cycle
+mode run
+set Reset 0
+step 50000
+EOF
+if grep SUCCESS runs/${file}_sim.out >/dev/null 2>&1
+then
+	exit 0
+else
+	exit 1
+fi
