711580e88e9904639b63403e10c3edb4a2ac4d6d
Karthi Srinivasan <karthisrinivasan98@gmail.com>
2025-09-25 16:17:00 -0400

    mangling fixes

--- ring_engine.cc
+++ ring_engine.cc
@@ -131,19 +131,26 @@ class RingSynth : public ActSynthesize {
     fprintf(_pp->fp, "\n// Total Bitwidth : %d\n", w);
     char name[10240];
     char mname[10240];
+    std::string ns = "";
+    if (d->getns() && d->getns() != ActNamespace::Global()) {
+      ns = d->getns()->Name(true);
+    }
     d->snprintActName(name, 10240);
-    mangle_data (mname, d);
+    ns = ns + name;
+    ActNamespace::Act()->msnprintf(mname, 10240, ns.c_str());
     const char *scn = config_get_string("synth.ring.struct_chan_name");;
     fprintf(_pp->fp, "defchan chan_%s <: chan(%s) (ring_chan<%d> %s) {}\n\n", 
-                      mname, name, w, scn);
+                      mname, ns.c_str(), w, scn);
   }
 
   void typeStructChan (char *buf, int sz, InstType *t) {
     InstType *td = TypeFactory::getChanDataType(t);
     Assert (td, "What");
     char name[10240];
-    mangle_it (name, td);
-    snprintf (buf, sz, "chan_%s", name);
+    td->sPrint(name, 10240);
+    char mname[10240];
+    ActNamespace::Act()->msnprintf(mname, 10240, name);
+    snprintf (buf, sz, "chan_%s", mname);
   }
 
   TinyForge *tf;
