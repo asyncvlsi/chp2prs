24f1286c64483856e6dfa9c01d57b857fd8f8856
Karthi Srinivasan <karthisrinivasan98@gmail.com>
2025-10-24 10:25:19 -0400

    Update chp_cost.cc

--- decomp/chp_cost.cc
+++ decomp/chp_cost.cc
@@ -33,6 +33,7 @@ void ChpCost::dump_actsim_conf(std::string conf_file, act_chp_lang_t *c, Process
   Assert (ff, "Could not open output file to write actsim configuration");
   fprintf(ff, "begin sim\n");
   fprintf(ff, "   begin chp\n");
+  fprintf(ff, "      int debug_metrics 0 # set to 1 to see delay association in actsim\n");
   fprintf(ff, "      int detailed_delay_annotation 1\n");
   fprintf(ff, "      begin decomp_%s<>\n", buf);
   fprintf(ff, "      int_table delays ");
